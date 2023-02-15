cd585254940c93b5987c06d3efb18afc3d970e4d
Ryuji Ogasawara <ryuji.ogasawara@autodesk.com>
2023-02-15 11:57:47 +0900

    remove public resources

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" type="image/x-icon" href="https://cdn.autodesk.io/favicon.ico" />
         <link rel="stylesheet" href="https://developer.api.autodesk.com/modelderivative/v2/viewers/7.*/style.css" type="text/css"></link>
       </Head>
       <body>
